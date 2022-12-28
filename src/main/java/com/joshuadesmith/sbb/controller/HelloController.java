790526198cd4126041d13ff3d9f26c0e5d96703e
Josh Smith <joshuadesmith@utexas.edu>
2022-12-28 12:09:51 -0600

    feat: added initial spring boot initializer code

---
+++ src/main/java/com/joshuadesmith/sbb/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.joshuadesmith.sbb.controller;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Data
+@RequiredArgsConstructor
+@RestController("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String hello(@RequestParam(required = false, defaultValue = "Anonymous") String name) {
+        return String.format("Hello, %s!", name);
+    }
+}
